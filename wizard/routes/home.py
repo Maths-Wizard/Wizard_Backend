7800a0c9a989e35900289e99ee5e4c18b24435a6
Certifieddonnie <mcdonaldsamure91@gmail.com>
2023-07-28 10:29:05 +0100

    [feat]: utilities functions

--- wizard/routes/home.py
+++ wizard/routes/home.py
@@ -1,2 +1,8 @@
 from flask import request
-from wizard import app
+from wsgi import app
+from wizard.utils.response import generate_response
+
+
+@app.route('/', methods=['GET'], strict_slashes=False)
+def welcome():
+    return generate_response(message='Welcome User!', status=200)
